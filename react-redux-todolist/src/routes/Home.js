c237b9c6b49f051de105f1a5ce091775eb14c5fb
dahye <dhk8233@gmail.com>
2023-02-02 14:54:47 +0900

    react-todolist-add,delete

--- react-redux-todolist/src/routes/Home.js
+++ react-redux-todolist/src/routes/Home.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addToDo, deleteToDo } from "../store";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [text, setText] = useState("");
+  const toDos = useSelector((state) => state);
 
-export default Home
+  const dispatch = useDispatch();
+
+  function onChange(e) {
+    setText(e.target.value);
+  }
+
+  function onSubmit(e) {
+    e.preventDefault();
+    dispatch(addToDo(text));
+    setText("");
+  }
+
+  function btnOnClick(e) {
+    const targetId = parseInt(e.target.parentNode.id);
+    dispatch(deleteToDo(targetId));
+  }
+
+  return (
+    <>
+      <h1>To Do</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={text} onChange={onChange} />
+        <button>Add</button>
+      </form>
+      <ul>
+        {toDos.map((toDo) => (
+          <li key={toDo.id} id={toDo.id}>
+            <Link to={`/${toDo.id}`}>{toDo.text}</Link>
+            <button onClick={btnOnClick}>DEL</button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default Home;
