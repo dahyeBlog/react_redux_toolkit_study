ba3defe909712acb1b4f0e961315e9c848a71718
dahye <dhk8233@gmail.com>
2023-02-02 11:00:13 +0900

    first commit

---
+++ react-redux-todolist/README.md
@@ -0,0 +1,8 @@
+## react 에서 사용되는 redux 
+- react 에서 redux를 사용하여 todo List를 만들면서 이해해보자.
+
+## 사용한 라이브러리
+- npm i react-dom
+- npm i react-router-dom
+- npm i react-redux     
+- npm i redux     
---
+++ react-redux-todolist/src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter as Routers, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+
+function App() {
+  return (
+    <Routers>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/:id" element={<Detail />}></Route>
+      </Routes>
+    </Routers>
+  );
+}
+
+export default App;
